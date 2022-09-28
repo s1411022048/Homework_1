8f7026ad4824387150683c8f468a30b6564256a3
s1411022048 <gagasan021116@gmail.com>
2022-09-28 15:17:27 +0800

    GG

--- HomeWork_B.js
+++ HomeWork_B.js
@@ -1,8 +1,34 @@
 var Readline = require("readline-sync");
+while (true){
+while (true){
+    var Height = Readline.question("Your Height(cm):");
 
-var Height = Readline.question("Your Height(cm):");
+    if(isNaN(Height)||Height < 0 || Height > 220){
+        console.log("Please Enter A Number Between 0 to 220");
+        continue;
+    }
+    else{
+        break;
+    }
+}
 console.log("Your Height is :" + Height + " cm!")
-var Weight = Readline.question("Your Weight(kg):");
+
+while (true){
+    var Weight = Readline.question("Your Weight(kg):");
+
+    if(isNaN(Weight)||Weight < 0 || Weight > 220){
+        console.log("Please Enter A Number Between 0 to 200");
+        continue;
+    }
+    else{
+        break;
+    }
+}
 console.log("Your Weight is :" + Weight + " kg!")
+
+
 var BMI = Weight/((Height/100)^2);
-console.log("BMI =" + BMI)
+console.log("BMI = " + BMI);
+console.log("-------------------------------");
+continue;
+}
